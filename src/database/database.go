9d89c6e7023c8c34293df05a4b695c3f9a5284e3
George Parsehyan <feanor306@live.com>
2024-10-23 14:47:23 +0200

    db creation

--- src/database/database.go
+++ src/database/database.go
@@ -8,36 +8,75 @@ import (
 	"github.com/feanor306/image_tagger/src/config"
 )
 
+// db handles database connection
 type db struct {
-	db   *sql.DB
+	conn *sql.DB
 	conf *config.Config
 }
 
+// dbInstance is a singleton db instance
 var dbInstance *db
 
 // GetDatabase creates or returns the database instance
-func GetDatabase(conf *config.Config) *db {
+func GetDatabase(conf *config.Config) (*db, error) {
 	if dbInstance != nil {
-		return dbInstance
+		return dbInstance, nil
 	}
 
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", conf.DbUser, conf.DbPassword, conf.DbHost, conf.DbPort, conf.DbName)
 
 	dbConn, err := sql.Open("pgx", connStr)
 	if err != nil {
-		log.Fatal(err)
-		panic(err)
+		return nil, err
 	}
 
 	dbInstance = &db{
-		db:   dbConn,
+		conn: dbConn,
 		conf: conf,
 	}
-	return dbInstance
+	return dbInstance, nil
 }
 
 // Close closes the database connection.
 func (db *db) Close() error {
 	log.Printf("Disconnected from database: %s", db.conf.DbName)
-	return db.db.Close()
+	return db.conn.Close()
+}
+
+// InitDatabase will create all the tables and indices
+func (db *db) InitDatabase() error {
+	_, err := db.conn.Exec(`
+		CREATE TABLE IF NOT EXISTS tags (
+			id text NOT NULL,
+			name text NOT NULL,
+			PRIMARY KEY (id)
+		);
+		CREATE UNIQUE INDEX IF NOT EXISTS tags_id_index ON tags USING btree (id text_pattern_ops);
+		CREATE UNIQUE INDEX IF NOT EXISTS tags_name_index ON tags USING btree (name text_pattern_ops);
+
+		CREATE TABLE IF NOT EXISTS images (
+			id text NOT NULL,
+			name text NOT NULL,
+			filename text NOT NULL,
+			PRIMARY KEY (id)
+		);
+		CREATE UNIQUE INDEX IF NOT EXISTS images_id_index ON images USING btree (filename text_pattern_ops);
+		CREATE INDEX IF NOT EXISTS images_name_index ON images USING btree (name text_pattern_ops);
+		CREATE INDEX IF NOT EXISTS images_filename_index ON images USING btree (filename text_pattern_ops);
+
+		CREATE TABLE IF NOT EXISTS image_tags (
+			image_id text NOT NULL,
+			tag_id text NOT NULL,
+			PRIMARY KEY (image_id, tag_id),
+			FOREIGN KEY (image_id) REFERENCES images (id),
+			FOREIGN KEY (tag_id) REFERENCES tags (id)
+		);
+		CREATE INDEX idx_image_tags_image_id ON image_tags (image_id);
+		CREATE INDEX idx_image_tags_tag_id ON image_tags (tag_id);
+	`)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
