05c45a972800b2f99b4b306db7a2d5fb01d00daf
George Parsehyan <feanor306@live.com>
2024-10-25 13:25:02 +0200

    assembled everything in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/feanor306/image_tagger/src/config"
+	"github.com/feanor306/image_tagger/src/database"
+	"github.com/feanor306/image_tagger/src/handler"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	conf := config.GetConfig()
+
+	db, err := database.GetDatabase(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.InitDatabase()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	srv := echo.New()
+
+	tagHandler := handler.TagHandler{
+		DB: db,
+	}
+	mediaHandler := handler.MediaHandler{
+		DB: db,
+	}
+
+	srv.GET("/", tagHandler.HandleTagShowAll)
+	srv.GET("/tags", tagHandler.HandleTagShowAll)
+	srv.POST("/tags", tagHandler.HandleTagCreate)
+	srv.GET("/media-new", mediaHandler.HandleMediaNew)
+	srv.POST("/media", mediaHandler.HandleMediaCreate)
+	srv.GET("/media", mediaHandler.HandleMediaShowAll)
+
+	srv.Static("/files", "data/files")
+
+	srv.Start(fmt.Sprintf(":%s", conf.SrvPort))
 }
